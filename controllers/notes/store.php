153b2a22363ed7c730042cd56d40eca0c42280d3
Nuri Demir <nuridemir34@hotmail.com>
2023-09-06 13:05:43 +0300

    ep 34 finish

---
+++ controllers/notes/store.php
@@ -0,0 +1,32 @@
+<?php
+
+use Core\Validator;
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$errors = [];
+
+
+if (!Validator::string($_POST['body'], 1, 100)) {
+    $errors['body'] = 'min 1 max 100 character';
+}
+
+if (!empty($errors)) {
+
+    return view("notes/create.view.php", [
+        'heading' => 'Create Note',
+        'errors' => $errors
+    ]);
+
+}
+
+$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+    'body' => $_POST['body'],
+    'user_id' => 1
+]);
+
+header("location:/notes");
+die();
+
